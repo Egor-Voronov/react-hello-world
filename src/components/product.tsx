a28f94b590132fce64fe13c1d4260a5c9b5c247a
javacripteaser88 <vip.egor.voronov.2016@mail.ru>
2022-10-27 06:50:18 +0300

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   package-lock.json
    	modified:   package.json
    	new file:   postcss.config.js
    	modified:   src/App.tsx
    	new file:   src/components/product.tsx
    	new file:   src/data/products.tsx
    	modified:   src/index.css
    	modified:   src/index.tsx
    	new file:   src/jsx.example.tsx
    	new file:   src/models.ts
    	new file:   tailwind.config.js

---
+++ src/components/product.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+import { IProduct } from "../models"
+
+interface ProductProps {
+    product: IProduct
+}
+
+export function Product (props: ProductProps) {
+    return (
+        <div
+        className="border py-2 px-4 rounded flex flex-col items-center mb-2"
+        >Product -
+        { props.product.title }
+        </div>
+    )
+}
