3c6315411f2af12525143ad4b4faf07b3055b95d
javacripteaser88 <vip.egor.voronov.2016@mail.ru>
2022-10-31 16:05:09 +0300

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   app1/src/App.tsx
    	new file:   app1/src/components/CreateProduct.tsx
    	new file:   app1/src/components/Error.tsx
    	new file:   app1/src/components/Modal.tsx
    	modified:   app1/src/components/product.tsx
    	new file:   app1/src/context/ModalContext.tsx
    	modified:   app1/src/hooks/product.ts
    	modified:   app1/src/index.tsx
    	modified:   app1/src/models.ts

--- app1/src/App.tsx
+++ app1/src/App.tsx
@@ -1,18 +1,34 @@
 import {Loader} from './components/Loader'
+import {Error} from './components/Error'
 import {Product} from './components/product'
 import { useProducts } from './hooks/product';
+import {Modal} from './components/Modal';
+import { CreateProduct } from './components/CreateProduct';
+import { useContext } from 'react';
+import { IProduct } from './models';
+import { ModalContext } from './context/ModalContext';
 
 function App() {
-  const { loading, error, products} = useProducts()
+  const { loading, error, products, addProduct} = useProducts()
+  const {modal, open, close} = useContext(ModalContext)
+
+  const createHandler = (product: IProduct) => {
+    close(false)
+    addProduct(product)
+  }
 
   return (
     <div className='container mx-auto max-w-2xl pt-5'>
       { loading && <Loader /> }
-      { error && <p className='text-center text-red-600'> {error} </p> }
+      { error && <Error error = {error} /> }
       { products.map(product => <Product product={product} key={product.id} />) }
 
-      {/* <Product product={products[0]} />
-      <Product product={products[1]} /> */}
+      { modal && <Modal title='Create new product' onClose={() => setModal(false)}>
+        <CreateProduct onCreate={createHandler} />
+      </Modal>}
+
+      <button className='fixed bottom-5 right-5 rounded-full bg-red-700 text-white text-2xl px-4 py-1/5'
+      onClick={() => setModal(true)}>+</button>
     </div>
   )
 };
---
+++ app1/src/components/CreateProduct.tsx
@@ -0,0 +1,67 @@
+import axios from "axios";
+import React, { useState } from "react";
+import { on } from "stream";
+import { IProduct } from "../models";
+import { Error } from "./Error";
+
+
+const productData: IProduct = {
+  title: "test product",
+  price: 13.5,
+  description: "lorem ipsum set",
+  image: "https://i.pravatar.cc",
+  category: "electronic",
+  rating: {
+    rate: 42,
+    count: 10,
+  }
+};
+
+interface CreateProductProps {
+    onCreate: (product: IProduct) => void
+}
+
+export function CreateProduct({ onCreate }: CreateProductProps) {
+  const [value, setValue] = useState("");
+  const [error, setError] = useState("");
+
+  const sumbitHandler = async (event: React.FormEvent) => {
+    event.preventDefault();
+    setError('')
+
+    if (value.trim().length === 0) {
+         setError('Please enter valid title.')
+         return
+    }
+
+    productData.title = value
+    const response =  await axios.post<IProduct>('https://fakestoreapi.com/products', productData)
+
+    onCreate(response.data)
+  };
+
+  const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+  };
+
+  return (
+    <form onSubmit={sumbitHandler}>
+      <input
+        type="text"
+        className="border py-2 px-4 mb-2 w-full outline-0"
+        placeholder="Enter product title..."
+        value={value}
+        onChange={changeHandler}
+      />
+
+    {error && <Error error = {error} /> }
+
+      <button
+        type="submit"
+        className="py-2 px-4 border bg-yellow-400 hover:bg-blue-400"
+      >
+        Create
+      </button>
+    </form>
+  );
+}
